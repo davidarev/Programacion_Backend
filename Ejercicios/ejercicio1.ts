2b0c9a786001b601d63de8360465a71c82afdb49
David Arévalo <72552800+davidarev@users.noreply.github.com>
2022-11-13 17:57:12 +0100

    Update ejercicio1.ts

--- Ejercicios/ejercicio1.ts
+++ Ejercicios/ejercicio1.ts
@@ -1,3 +1,10 @@
+/*
+Entregar un archivo ejercicio1.ts
+Partiendo del array
+const arr = [ 2,3,4,2,4,3,32,3,3,5,3,3,3,2,212,2,3];
+Hacer otro array que tenga solo los impares y sin repetirse.
+*/
+
 const array = [1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 3, 2, 1]
 
 //ejercicio1.ts SIMPLE
